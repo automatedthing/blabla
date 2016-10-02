aa1bae8ca261379afbcaea695c5e2ae184760eaf
Lukluk <luklukaha@gmail.com>
2016-10-02 23:53:02 +0700

    fix js

--- source/instagram.js
+++ source/instagram.js
@@ -1,25 +1,29 @@
 var Nightmare = require('nightmare');
 var request = require('request');
+const realMouse = require('nightmare-real-mouse');
+realMouse(Nightmare);
 
 module.exports = {
     login: function() {
         var nightmare = Nightmare({
-            show: false
+            show: true
         })
         nightmare
             .goto('http://instagram.com')
             .inject('js', 'jquery.js')
-            .wait('#react-root > section > main > article > div > div:nth-child(2) > p > a')
-            .click('#react-root > section > main > article > div > div:nth-child(2) > p > a')
+            .wait('main > article > div > div:nth-child(2) > p > a')
+            .click('main > article > div > div:nth-child(2) > p > a')
+            .screenshot('test.png')
             .wait('[name=username]')
             .type('[name=username]', 'indo.pictures')
             .type('[name=password]', 'Cipc0pmimiig')
             .click('form button')
             .wait('.coreSpriteSearchIcon')
-            .then(function() {
-                nightmare.end()
-            })
             .end()
+            .run(function(err, x) {
+                if (err) return console.log(err);
+                console.log("SUCCESS LOGIN");
+            });
     },
     profile: function(req, res) {
         var self = this
@@ -70,6 +74,116 @@ module.exports = {
                         running = false
                         nightmare
                             .evaluate(function() {
+                              var media = {}
+                              media['0'] = _sharedData.entry_data.ProfilePage[0].user.media.nodes
+                              jQuery('body').append('<mediax style="display:none">{}</mediax>')
+                              jQuery('mediax').html(JSON.stringify(media))
+                                function replaceXHR(){
+                                    (function(window,debug){
+
+                                        function args(a){
+                                            var s = "";
+                                            for(var i = 0; i < a.length; i++) {
+                                                s += "\t\n[" + i + "] => " + a[i];
+                                            }
+                                            return s;
+                                        }
+                                        var _XMLHttpRequest = window.XMLHttpRequest;
+
+                                        window.XMLHttpRequest = function() {
+                                            this.xhr = new _XMLHttpRequest();
+                                        }
+
+                                        // proxy ALL methods/properties
+                                        var methods = [
+                                            "open",
+                                            "abort",
+                                            "setRequestHeader",
+                                            "send",
+                                            "addEventListener",
+                                            "removeEventListener",
+                                            "getResponseHeader",
+                                            "getAllResponseHeaders",
+                                            "dispatchEvent",
+                                            "overrideMimeType"
+                                        ];
+                                        methods.forEach(function(method){
+                                            window.XMLHttpRequest.prototype[method] = function() {
+                                                if (debug) console.log("ARGUMENTS", method, args(arguments));
+                                                if (method == "open") {
+                                                    this._url = arguments[1];
+                                                }
+                                                return this.xhr[method].apply(this.xhr, arguments);
+                                            }
+                                        });
+
+                                        // proxy change event handler
+                                        Object.defineProperty(window.XMLHttpRequest.prototype, "onreadystatechange", {
+                                            get: function(){
+                                                // this will probably never called
+                                                console.log('get')
+                                                return this.xhr.onreadystatechange;
+                                            },
+                                            set: function(onreadystatechange){
+                                              console.log('set')
+                                                var that = this.xhr;
+                                                var realThis = this;
+                                                that.onreadystatechange = function(){
+                                                    // request is fully loaded
+                                                    if (that.readyState == 4) {
+                                                        if (debug) console.log("RESPONSE RECEIVED:", typeof that.responseText == "string" ? that.responseText.length : "none");
+                                                        // there is a response and filter execution based on url
+                                                        if (that.responseText && realThis._url.indexOf("whatever") != -1) {
+                                                            window.myAwesomeResponse = that.responseText;
+                                                        }
+                                                    }
+                                                    onreadystatechange.call(that);
+                                                };
+                                            }
+                                        });
+
+                                        var otherscalars = [
+                                            "onabort",
+                                            "onerror",
+                                            "onload",
+                                            "onloadstart",
+                                            "onloadend",
+                                            "onprogress",
+                                            "readyState",
+                                            "responseText",
+                                            "responseType",
+                                            "responseXML",
+                                            "status",
+                                            "statusText",
+                                            "upload",
+                                            "withCredentials",
+                                            "DONE",
+                                            "UNSENT",
+                                            "HEADERS_RECEIVED",
+                                            "LOADING",
+                                            "OPENED"
+                                        ];
+                                        otherscalars.forEach(function(scalar){
+                                            Object.defineProperty(window.XMLHttpRequest.prototype, scalar, {
+                                                get: function(){
+                                                    if(this.xhr.responseText)
+                                                    {
+                                                      var o = JSON.parse(this.xhr.responseText)
+                                                      if(o && o.media.page_info)
+                                                      media[o.media.page_info.start_cursor]=o.media.nodes
+                                                      jQuery('mediax').html(JSON.stringify(media))
+                                                    }
+                                                    return this.xhr[scalar];
+                                                },
+                                                set: function(obj){
+
+                                                    this.xhr[scalar] = obj;
+                                                }
+                                            });
+                                        });
+                                    })(window,false);
+                                }
+                                replaceXHR()
 
                                 var data = {}
                                 data.total_comments = 0
@@ -119,6 +233,7 @@ module.exports = {
         nightmare
             .goto('http://instagram.com/' + req.params.user)
             .inject('js', 'jquery.js')
+            .inject('js', 'mine.js')
             .on('console', function(type, arg) {
                 console.log("LOOOG", type, arg)
             })
@@ -146,7 +261,7 @@ module.exports = {
     },
     GetFollow: function(req, res, mode) {
         var nightmare = Nightmare({
-            show: false
+            show: true
         })
         var running = true;
 
@@ -182,7 +297,7 @@ module.exports = {
                                 console.error('Search failed:', error);
                             });
                     } else {
-                        setTimeout(done, 10)
+                        setTimeout(done, 500)
                     }
                 })
         }
